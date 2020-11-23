d6f438eb87a7e0d369c45072a39cd44ea9d03a87
Emily Willow <emily.shapiera@gmail.com>
2020-11-23 16:33:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,17 @@
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
+<<<<<<< HEAD
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
+puts "-----------"
+puts " #{board[3]}  | #{board[4]}  | #{board[5]}  "
+puts "-----------"
+puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
+>>>>>>> 4d7c81b09db7ed511f69331e4ed1e6b2a892487b
 end
